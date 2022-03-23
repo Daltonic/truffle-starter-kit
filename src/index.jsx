7b7328e537fa6fdb0109b449533bb07253d29bb3
Daltonic <darlingtongospel@gmail.com>
2022-03-23 11:59:25 +0100

    updates the index, removes extra codes

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
-import '@material-tailwind/react/tailwind.css'
-import { initCometChat } from './cometChat'
 import App from './App'
 
-initCometChat().then(() => {
-  ReactDOM.render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>,
-    document.getElementById('root')
-  )
-})
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root')
+)
