d40fa4ff7774b4dce28715b6135b2d4641e84a6f
Jack Turner <34825609+neverekt@users.noreply.github.com>
2023-03-16 06:10:47 +0000

    added goerli testnet and set up to refer to env file for mnemonic, url and api for alchemy

--- truffle-config.js
+++ truffle-config.js
@@ -1,5 +1,3 @@
-// require('babel-register')
-// require('babel-polyfill')
 require('dotenv').config()
 const HDWalletProvider = require('@truffle/hdwallet-provider')
 
@@ -11,15 +9,24 @@ module.exports = {
       port: 7545,
       network_id: '*', // Match any network id
     },
-    rinkeby: {
-      provider: () =>
-        new HDWalletProvider(process.env.SECRET_KEY, process.env.ENDPOINT_URL),
-      network_id: 4, // Rinkeby's id
-      gas: 5500000, // Rinkeby has a lower block limit than mainnet
-      confirmations: 2, // # of confs to wait between deployments. (default: 0)
+    goerli: {
+      provider: () => {
+        return new HDWalletProvider(process.env.SECRET_KEY, process.env.ENDPOINT_URL + process.env.DEPLOYER_KEY)
+      },
+      network_id: 5, // Goerli's id
+      confirmations: 2, // # of confirmations to wait between deployments. (default: 0)
       timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
       skipDryRun: true, // Skip dry run before migrations? (default: false for public nets )
-    },
+    }
+    // rinkeby: {
+    //   provider: () =>
+    //     new HDWalletProvider(process.env.SECRET_KEY, process.env.ENDPOINT_URL),
+    //   network_id: 4, // Rinkeby's id
+    //   gas: 5500000, // Rinkeby has a lower block limit than mainnet
+    //   confirmations: 2, // # of confs to wait between deployments. (default: 0)
+    //   timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
+    //   skipDryRun: true, // Skip dry run before migrations? (default: false for public nets )
+    // },
   },
   contracts_directory: './contracts/',
   contracts_build_directory: './src/shared/abis/',
